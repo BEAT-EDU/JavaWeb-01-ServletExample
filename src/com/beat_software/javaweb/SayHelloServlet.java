9a87c966279bd6dc874f3057b48d3f1bec14b8aa
beat-edu <pyisoe@Pyis-MacBook-Pro.local>
2015-09-23 14:02:43 +0630

    added dynamic html generation logic to SayHelloServlet.java. added user input html page, person_form.html

--- src/com/beat_software/javaweb/SayHelloServlet.java
+++ src/com/beat_software/javaweb/SayHelloServlet.java
@@ -1,37 +1,70 @@
 package com.beat_software.javaweb;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class SayHelloServlet
- */
 public class SayHelloServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+	private static final long serialVersionUID = 1L;       
+
     public SayHelloServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
+		
+		String name = request.getParameter("name");
+		String gender = request.getParameter("gender");
+		String[] hobbies = request.getParameterValues("hobby");
+		
+		System.out.println("--------name" + name);
+		System.out.println("--------gender" + gender);
+		System.out.println("--------hobbies" + hobbies);
 
+		String title = "";
+		if("M".equals(gender)){
+			title = "Mr. ";
+		}
+		
+		if("F".equals(gender)){
+			title = "Ms. ";
+		}
+		
+		StringBuilder hobbiesStr = new StringBuilder("");
+		if(hobbies.length > 2){
+			for(int i = 0; i < hobbies.length - 2; i++){
+				hobbiesStr.append(hobbies[i]).append(", ");
+			}
+			hobbiesStr.append(hobbies[hobbies.length - 2]).append( " and ").append(hobbies[hobbies.length - 1]);
+		}
+		
+		if(hobbies.length == 2){
+			hobbiesStr.append(hobbies[0]).append(" and ").append(hobbies[1]);
+		}
+		
+		if(hobbies.length == 1){
+			hobbiesStr.append(hobbies[0]);
+		}		
+		
+		//construct html string
+		StringBuilder html = new StringBuilder("");
+		html.append("<html>");
+		html.append(	"<body>");
+		html.append(		" <h1 align=center>Glad to know your hobbies!</h1>");
+		html.append(				"Hello ").append(title).append(name).append(". <br> <br>");
+		html.append(				"Glad to know you love ");
+		html.append(				hobbiesStr).append(".");
+		html.append(		"<br>");
+		html.append(	"</body>"); 
+		html.append("</html>");
+		
+		PrintWriter out = response.getWriter();
+		out.println(html);
+				
+				
+	}
 }
